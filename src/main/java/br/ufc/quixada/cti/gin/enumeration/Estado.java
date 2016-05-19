f03d7ac8bbf9c621b3ff67c3eaf2fba052c7cc89
Roberto Junior <robertojunior085@gmail.com>
2016-05-19 10:49:54 -0300

    criação: enum estado; pessoa class; pessoaentitylistener class

---
+++ src/main/java/br/ufc/quixada/cti/gin/enumeration/Estado.java
@@ -0,0 +1,21 @@
+package br.ufc.quixada.cti.gin.enumeration;
+
+public enum Estado {
+
+	AC("Acre"), AL("Alagoas"), AP("Amapa"), AM("Amazonas"), BA("Bahia"), 
+	CE("Ceará"), DF("Distrito Federal"), ES("Espirito Santo"), GO("Goiás"), MA("Maranhão"), 
+	MT("Mato Grosso"), MS("Mato Grosso do Sul"), MG("Minas Gerais"), PA("Pará"), PB("Paraíba"), 
+	PR("Paraná"), PE("Pernambuco"), PI("Piauí"), RJ("Rio de Janeiro"), RN("Rio Grande do Norte"), 
+	RS("Rio Grande do Sul"), RO("Rondonia"), RR("Roraima"), SC("Santa Catarina"), SP("São Paulo"), 
+	SE("Sergipe"), TO("Tocantins");
+	
+	private String tipo;
+	
+	private Estado(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public String getTipo(){
+		return tipo;
+	}
+}
---
+++ src/main/java/br/ufc/quixada/cti/gin/model/Pessoa.java
@@ -0,0 +1,158 @@
+package br.ufc.quixada.cti.gin.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
+
+import br.ufc.quixada.cti.gin.enumeration.Estado;
+
+@Entity
+@EntityListeners(PessoaEntityListener.class)
+@Table(uniqueConstraints = @UniqueConstraint( columnNames = { "id", "cpf" } ))
+public class Pessoa {
+
+	public Pessoa() {
+		super();
+	}
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Transient
+	private String email;
+	
+	@Transient
+	private String nome;
+	
+	private String rg;
+
+	private String cpf;
+
+	@Column(name = "datanascimento", columnDefinition="DATE")
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+	
+	private String nacionalidade;
+	
+	private String naturalidade;
+	
+	@Enumerated(EnumType.STRING)
+	private Estado uf;
+
+	private String sexo;
+	
+	private String telefone;
+	
+	private String estadoCivil;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+
+	public Estado getUf() {
+		return uf;
+	}
+
+	public void setUf(Estado uf) {
+		this.uf = uf;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	
+	
+	
+}
